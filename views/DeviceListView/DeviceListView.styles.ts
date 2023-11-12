fca75176c260c2365765f7f14d8ff7ef21a925d0
Phabes <quinlanvos1234@gmail.com>
2023-11-12 14:04:13 +0100

    flatlist deleted due to error with flatlist inside scrollview [HOME-53]

--- views/DeviceListView/DeviceListView.styles.ts
+++ views/DeviceListView/DeviceListView.styles.ts
@@ -2,13 +2,13 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../.storybook/theme';
 
 export const styles = StyleSheet.create({
-  separatingElement: {
-    marginVertical: theme.spacing(2),
-  },
   reload: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     gap: theme.spacing(2),
+  },
+  devicesList: {
+    gap: theme.spacing(3),
   },
 });
