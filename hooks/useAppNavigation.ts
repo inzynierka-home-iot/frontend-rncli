3347be4d42a2ccf2081c6c121248182467f3f4bc
Phabes <quinlanvos1234@gmail.com>
2023-11-28 08:43:38 +0100

    Back to wrong import [HOME-63]

--- hooks/useAppNavigation.ts
+++ hooks/useAppNavigation.ts
@@ -1,4 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import { RootNavigationProps } from '../types';
+// TODO repair import
+import { RootNavigationProps } from '../App';
 
 export const useAppNavigation = () => useNavigation<RootNavigationProps>();
