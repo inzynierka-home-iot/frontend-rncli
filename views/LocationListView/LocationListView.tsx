9fa0646a364a2237e4a81d69cb70a8d0e40bdd11
matuesz-kleszcz <kleeeszcz@gmail.com>
2023-11-30 19:28:51 +0100

    fix: HOME-48 missing dependencies

--- views/LocationListView/LocationListView.tsx
+++ views/LocationListView/LocationListView.tsx
@@ -36,7 +36,7 @@ export const LocationListView = () => {
   useFocusEffect(
     useCallback(() => {
       retrieveAvailableLocations();
-    }, [retrieveAvailableLocations]),
+    }, [botFatherAccessHash, botFatherId]),
   );
 
   return (
