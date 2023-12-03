6f5c1288e5f786a79969ddfdb3369916821f24f8
Phabes <quinlanvos1234@gmail.com>
2023-12-03 13:33:43 +0100

    Letter case fixed and select index hook changed [HOME-70]

--- .storybook/stories/Select/useSelectValue.tsx
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from "react"
-
-export const useSelectValue = (defaultValue = 0): [number, (value: number) => void] => {
-  const [selectValue, setSelectValue] = useState(defaultValue)
-
-  const onChange = useCallback((value: number) => setSelectValue(value), [setSelectValue])
-
-  return [selectValue, onChange]
-}
