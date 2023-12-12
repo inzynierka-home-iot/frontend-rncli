2300b70488d3974d7f4b989d5924b5b3c22d9999
Phabes <quinlanvos1234@gmail.com>
2023-12-13 00:57:04 +0100

    TODO deleted [HOME-72]

--- hooks/useAppNavigation.ts
+++ hooks/useAppNavigation.ts
@@ -1,6 +1,4 @@
 import { useNavigation } from '@react-navigation/native';
 import { RootNavigationProps } from '../types';
-// TODO repair import
-// import { RootNavigationProps } from '../App';
 
 export const useAppNavigation = () => useNavigation<RootNavigationProps>();
